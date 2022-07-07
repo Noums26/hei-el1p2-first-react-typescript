b389e310f5fdafd3847787962b978f8aa942dfa1
Rojotiana Nomena <hei.nomena@gmail.com>
2022-07-07 16:54:09 +0300

    feat: counter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+ 
+const Counter: React.FC<{}> = () => {
+  const [count, setCount] = useState<number>(0);
+  return (<button onClick={():void => {
+    setCount(count + 1)
+  }} >Count: {count}</button>);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
